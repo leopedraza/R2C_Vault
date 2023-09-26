f4bce36ef46817451782dd7886124074f8cf07e9
Leonardo Pedraza <88497191+leopedraza@users.noreply.github.com>
2023-09-26 02:01:17 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "CipherText" {
     value =  <<EOT
 Este es el texto cifrado:
 ${oci_kms_encrypted_data.Encrypted_data_R2C.ciphertext}
-para obtener el texto plano solo necesitas el cloud Shell y el OCID de tu llave.
+para obtener el texto plano solo necesitas el cloud Shell, el OCID de tu llave y el endpoint.
 https://docs.oracle.com/en-us/iaas/tools/oci-cli/3.33.0/oci_cli_docs/cmdref/kms/crypto/decrypt.html
 Ten en cuenta que tu resultado es base64
 EOT
@@ -12,3 +12,6 @@ output "OCID_llave" {
     value = oci_kms_key.R2C_key.id
 }
 
+output "endpoint" {
+    value = oci_kms_vault.R2C_vault.crypto_endpoint
+}
