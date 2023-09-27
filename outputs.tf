5c43db6a86a44c6ea2cb207caa721ce3e90fce3b
Leonardo Pedraza <88497191+leopedraza@users.noreply.github.com>
2023-09-27 17:44:09 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ Este es el texto cifrado:
 ${oci_kms_encrypted_data.Encrypted_data_R2C.ciphertext}
 -------------------------------------------------------
 
-Para obtener el texto plano solo necesitas el cloud Shell, el OCID de tu llave y el endpoint.
+Para obtener el texto plano solo necesitas el cloud Shell, el key-id y el endpoint.
 
 Puedes consultar la informacion sobre el comando de descifrado aqui: 
 https://docs.oracle.com/en-us/iaas/tools/oci-cli/3.33.0/oci_cli_docs/cmdref/kms/crypto/decrypt.html
